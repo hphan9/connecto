402167c09121cd23a11dfbb2c98f5d4422495424
hphan9 <emilyhuyenphan@gmail.com>
2024-12-04 23:18:33 -0500

    Set up test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,8 @@
-import express from 'express';
+import app from './app';
 
-const app = express();
-
-app.get("*", (req, res) => {
-  res.send({});
+app.get('*', (req, res) => {
+  res.status(200).send({});
 });
-
-
-
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
