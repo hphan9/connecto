0ab132750a78e8a1917548a39edc394d65018772
hphan9 <emilyhuyenphan@gmail.com>
2024-12-22 21:54:28 -0500

    Add Sidebar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
 import "./App.css";
-function App() {
-  const [count, setCount] = useState(0);
+import Sidebar from "./components/common/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/LoginPage";
+import SignUpPage from "./pages/auth/SignupPage";
 
-  return <div className="flex max-w-6x1 mx-auto"></div>;
+// todo: move routes to seperate file
+function App() {
+  return (
+    <div className="flex max-w-6x1 mx-auto">
+      {/*Common component*/}
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,19 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home/HomePage.tsx";
-import LoginPage from "./pages/auth/LoginPage.tsx";
-import SignUpPage from "./pages/auth/SignupPage.tsx";
 
-// todo: move routes to seperate file
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
