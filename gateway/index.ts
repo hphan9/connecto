32dccd753cca72ac209ba65a046e8fa5287f5f48
hphan9 <emilyhuyenphan@gmail.com>
2025-01-26 23:50:27 -0500

    change Microservices endpoint in api gateway

--- gateway/index.ts
+++ gateway/index.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 //proxy will redirect request coming to api gateway based on endpoint
 import proxy from 'express-http-proxy';
 
+dotenv.config();
 const app= express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', proxy('http://localhost:8001'));
-app.use('/api/posts', proxy('http://localhost:8002'));
-app.use('/api/timeline', proxy('http://localhost:8003'));
-app.use('/api/users', proxy('http://localhost:8004'));
+app.use('/api/auth', proxy(`http://${process.env.AUTH_SERVICE}`));
+app.use('/api/posts', proxy(`http://${process.env.POST_SERVICE}`));
+app.use('/api/timeline', proxy(`http://${process.env.TIMELINE_SERVICE}`));
+app.use('/api/users', proxy(`http://${process.env.USER_SERVICE}`));
 
 app.listen(8000,()=>{
     console.log('API Gateway is listening to Port 8000');
